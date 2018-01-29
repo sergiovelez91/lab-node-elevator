f4e3e5eaea87a694e0d0de20568e6f36db93ad0d
Sergio <sergiovelez.svg@gmail.com>
2018-01-29 20:46:36 +0100

    version 1

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,64 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.passengers = [];
+    this.waitingList = [];
+    this.direction = "up";
+    this.setInterval;
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+   this.setInterval = setInterval(() => {
+      this.update();
+      this.floorUp();
+    }, 1000);
+  }
+  stop() {
+    let stopSetInterval = setInterval(update, 1000);
+    clearInterval(stopInterval);
+  }
+  update() {
+   return this.log();
+  }
+  _passengersEnter() {
+    for(let i =0; i<waitingList.lenth;i++)
+        if(this.waitingList[i].originFloor == this.floor) {
+          this.passengers.push(this.waitingList[i]);
+          this.requests.push(this.waitingList[i].destinationFloor);
+          this. waitingList.splite(i,1)
+      
+    }
+
+  }
+  _passengersLeave() {}
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR && this.direction == "up" ) {
+      this.floor++;
+     } else {
+      this.direction = "down";
+      this.floorDown()
+    }
+}
+
+floorDown() {
+  if (this.floor > 0 && this.direction == "down") {
+    this.floor--;
+  } else {
+    this.direction = "up";
+    this.floorUp();
+  }
+}
+  call(person) {
+    this.waitingList.push(person);
+    this.request.push(originFloor);
+  }
+  log() {
+    console.log(this.floor);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,7 @@
-const Elevator = require('./elevator.js');
+const Elevator = require("./elevator.js");
+const Person = require("./elevator.js");
+
+let elevator1 = new Elevator();
+let person1 = new Person ("Pepe",1,4);
+
+console.log()
